a37bb02c777313847f9b0cce7af5a38e72426c16
asile12 <elisa.sakamoto@zenkigen.jp>
2020-10-24 21:16:55 +0900

    Edit action

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
+
+const context = github.context
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const repoToken: string = core.getInput('repo-token', {required: true})
+    // core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    // await wait(parseInt(ms, 10))
+
+    core.info(`context: ${context ? JSON.stringify(context) : ''}`)
 
     core.setOutput('time', new Date().toTimeString())
   } catch (error) {
